f84f8e761349573f65affd754c1ccfc0e4b8c06b
Juun <blacklily7513@gmail.com>
2026-03-06 09:45:48 +0900

    revert(plugin): Remove error query

--- packages/typescript/src/query.ts
+++ packages/typescript/src/query.ts
@@ -1,17 +1,15 @@
 import { merge, normalize } from "@juun-roh/spine/utils/query";
 
 import classQuery from "./queries/class.scm";
-import errorQuery from "./queries/error.scm";
 import functionQuery from "./queries/function.scm";
 import importQuery from "./queries/import.scm";
 
 const queries: Record<string, string> = {
   class: normalize(classQuery),
-  error: normalize(errorQuery),
   function: normalize(functionQuery),
   import: normalize(importQuery),
 };
 
-const queryString = merge(classQuery, errorQuery, functionQuery, importQuery);
+const queryString = merge(classQuery, functionQuery, importQuery);
 
 export { queries, queryString };
