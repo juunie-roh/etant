12161932ec7006c2378e6a2929dc5467e74febff
Juun <blacklily7513@gmail.com>
2026-03-10 17:41:29 +0900

    revert(plugin): Remove queries for call detection

--- packages/typescript/src/queries/statement/if.scm
+++
@@ -1,7 +0,0 @@
-;; if statements
-;; if @condition @body @else
-(if_statement
-  condition: (parenthesized_expression) @condition
-  consequence: (statement) @body
-  alternative: (else_clause)? @else
-) @if
--- packages/typescript/src/queries/statement/iife.scm
+++
@@ -1,8 +0,0 @@
-;; Immediately Invoked Function Expression (IIFE) statements
-;; (function () {})();
-;; (() => {})();
-(expression_statement
-  (call_expression
-    function: (parenthesized_expression) @iife
-  )
-)
