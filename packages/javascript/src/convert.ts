ef4a09e2157769617220719a8f979f88ce6c871c
Juun <blacklily7513@gmail.com>
2026-03-24 11:32:10 +0900

    feat(plugin): Add if handler to convert config

--- packages/javascript/src/convert.ts
+++ packages/javascript/src/convert.ts
@@ -1,3 +1,4 @@
+import ifHandler from "./handlers/anonymous/if";
 import iifeHandler from "./handlers/anonymous/iife";
 import importHandler from "./handlers/binding/import";
 import memberHandler from "./handlers/binding/member";
@@ -8,6 +9,7 @@ import methodHandler from "./handlers/scope/method";
 import type { ConvertConfig } from "./types";
 
 export const convertConfig = {
+  if: ifHandler,
   iife: iifeHandler,
   import: importHandler,
   member: memberHandler,
